b27b26d8be683d9956b04eb58eaffcd9021bb1b5
gerges <georgefarag128@gmail.com>
2026-01-17 23:37:55 +0200

    add: request handler

--- api/index.ts
+++ api/index.ts
@@ -1,24 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { AppModule } from '../src/app.module';
 import helmet from 'helmet';
 
-let cachedServer;
-async function bootstrap() {
+let cachedServer: express.Express;
+
+async function bootstrap(): Promise<express.Express> {
   if (!cachedServer) {
     const server = express();
     const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-    app.enableCors();
     app.setGlobalPrefix('api/v1');
-
     app.use(helmet({}));
     app.enableCors({
-      origin: 'http://localhost:3000',
+      origin: '*',
     });
     await app.init();
     cachedServer = server;
   }
   return cachedServer;
 }
-export default bootstrap;
+
+export default async function handler(req: Request, res: Response) {
+  const server = await bootstrap();
+  server(req, res);
+}
